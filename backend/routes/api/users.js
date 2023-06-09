eb47b2da7cbd4ab4670d61354deaeec4ea793f2f
Jon <jon.ezana1@gmail.com>
2023-06-09 09:12:17 -0400

    Add firstName and lastName properties to User model, Users migration file, and api routes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -26,6 +26,12 @@ const validateSignup = [
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
     .withMessage('Password must be 6 characters or more.'),
+  check('firstName')
+    .exists({checkFalsy: true})
+    .withMessage('Please provide a first name.'),
+  check('lastName')
+    .exists({checkFalsy: true})
+    .withMessage('Please provide a last name.'),
   handleValidationErrors
 ];
 
@@ -34,14 +40,16 @@ router.post(
   '',
   validateSignup,
   async (req, res) => {
-    const { email, password, username } = req.body;
+    const { email, password, username, firstName, lastName } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
-    const user = await User.create({ email, username, hashedPassword });
+    const user = await User.create({ email, username, hashedPassword, firstName, lastName });
 
     const safeUser = {
       id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
       email: user.email,
-      username: user.username,
+      username: user.username
     };
 
     await setTokenCookie(res, safeUser);
