03eda36cc20c1a007422be248a11c2b502dbde58
Jon <jon.ezana1@gmail.com>
2023-06-13 12:07:39 -0400

    troubleshooting spots.js

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,8 +9,9 @@ const router = express.Router();
 
 router.get('', async (req, res) => {
     const allSpots = await Spot.findAll();
-    res.json(allSpots)
-})
+    const Spots = allSpots.map(spot => spot.toJSON());
+    res.json({Spots})
+});
 
 
 module.exports = router;
