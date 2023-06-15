9ad50f576f1111039aeadc498140ee626347f5ea
Jon <jon.ezana1@gmail.com>
2023-06-14 22:28:11 -0400

    Delete Review Image working in local env

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,7 +3,8 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
-const spotImagesRouter = require('./spotImages.js')
+const spotImagesRouter = require('./spotImages.js');
+const reviewImagesRouter = require('./reviewImages.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 // Connect restoreUser middleware to the API router
@@ -21,6 +22,8 @@ router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 
 router.use('/spot-images', spotImagesRouter);
+
+router.use('/review-images', reviewImagesRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
---
+++ backend/routes/api/reviewImages.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { Spot, Review, User, SpotImage, ReviewImage } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const {Sequelize} = require('sequelize');
+const { restoreUser, requireAuth } = require('../../utils/auth');
+
+const router = express.Router();
+
+// Delete review image
+router.delete('/:reviewImgId', restoreUser, requireAuth, async (req, res) => {
+    let img = await ReviewImage.findByPk(req.params.reviewImgId, {
+        include: [{model: Review, include: [{model: User, attributes: ['id']}]}]
+    });
+    if (!img) {
+        res.statusCode = 404;
+        res.json({message: "Review Image couldn't be found"})
+    } else {
+        let image = img.toJSON();
+        console.log(image)
+        if (req.user.id !== image.Review.User.id) {
+            res.statusCode = 403;
+            res.json({message: "Forbidden"})
+        } else {
+            await img.destroy();
+            res.statusCode = 200;
+            res.json({message: "Successfully deleted"})
+        }
+    }
+})
+
+module.exports = router;
